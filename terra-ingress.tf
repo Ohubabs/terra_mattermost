f344563a1972154f4106e665ae7a1f13f587290a
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-12-12 14:17:27 -0800

    Create terra-ingress.tf

---
+++ terra-ingress.tf
@@ -0,0 +1,77 @@
+resource "kubernetes_ingress_v1" "mattermost" {
+  wait_for_load_balancer = true
+  metadata {
+    name = "mattermost"
+    namespace = "default"
+    annotations = {
+        "alb.ingress.kubernetes.io/load-balancer-name" = "mattermost"
+        "alb.ingress.kubernetes.io/scheme"             = "internet-facing"
+        "alb.ingress.kubernetes.io/target-type"        = "ip"                                       
+        "alb.ingress.kubernetes.io/certificate-arn" = "arn:aws:acm:us-west-2:083772204804:certificate/58e604e3-40aa-4644-b0ad-8663f8e67c2a"
+  }
+  }
+  spec {
+    ingress_class_name = "alb"
+    default_backend {
+      service {
+        name = "mattermost-team-edition"
+        port {
+          number = 80
+        }
+      }
+    }
+    rule {
+      host = "mattermost.devopsnetwork.net"  
+      http {
+        path {
+          path = "/"
+          path_type = "Exact"
+          backend {
+            service {
+              name = "mattermost-team-edition"
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+##NGINX
+
+#resource "kubernetes_ingress_v1" "mattermost-ingress" {
+#  wait_for_load_balancer = true
+#  metadata {
+#    name = "mattermost"
+#    namespace = "default"
+#    annotations = {
+#        "cert-manager.io/cluster-issuer" = "mattermost-issuer"
+#  }
+#  }
+#  spec {
+#    ingress_class_name = "nginx"
+#    tls {
+#      secret_name = "mattermost-secret"
+#      hosts = ["matter.devopsnetwork.net"] 
+#    }
+#    rule {
+#      host = "matter.devopsnetwork.net"  
+#      http {
+#        path {
+#          path = "/"
+#          backend {
+#            service {
+#              name = "mattermost-team-edition"
+#              port {
+#                number = 80
+#              }
+#            }
+#          }
+#        }
+#      }
+#    }
+#  }
+#}
